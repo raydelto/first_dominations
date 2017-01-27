6d5651acc6f3b41bb3205c00432ed34ec2e9f07e
ray <ray@10.250.254.96>
2017-01-27 16:36:06 -0400

    Adding content to Ray's adventure

--- level0/adventure0.py
+++ level0/adventure0.py
@@ -13,10 +13,10 @@ def start():
     credits()
     print "You are in a dark forest, you have no idea how did you get there.  The place is scary, looks like a cemetery."
     print "Two shadows suddenly approach you.  You now have two gentlemen in front of you: Ray and Mitch"
-    print "Mitch told an uncomprensible joke, no one laugh, then told you that they are saving souls, you have to pick someone to guide you through your quest."
+    print "Mitch told a corny joke, no one laugh, then told you that they are saving souls, you have to pick someone to guide you through your quest."
     print "Minutes in silence have passed, you're in shock incapable of making a decision, Ray approaches you and tell you: "
-    print '"Listen wiseman, just pick randomly any of us , or just try to reach an exit by yourself, but we need to hurry, they are coming"'
-    print " -- They ?, who are they?. You asked with no clue of what are this gentlemen talking nor how did you get that awful place"
+    print '"Listen , just pick randomly any of us , or just try to reach an exit by yourself, but we need to hurry, they are coming"'
+    print " -- They ?, who are they?. You asked with no clue of what are this gentlemen talking nor how did you get to that awful place"
     print "\n\n\n"
     print"Time for a decision has arrived.  What would you like to do?"
     choice = 0
@@ -27,7 +27,7 @@ def start():
         elif choice == 2:
             ray_adventure.start()
         elif choice == 3:
-            print "You ran away, fall to the ground, and suddenly feel a sharped object stabbing you repeteadly, everything is blurry, everything is black..."
+            print "You ran away, fall to the ground, and suddenly feel a sharpened object stabbing you repeteadly, everything is blurry, everything is black..."
             print "\nGAME OVER\n"
         else:
             print "Please select a valid option"    
--- level0/ray_adventure.py
+++ level0/ray_adventure.py
@@ -4,5 +4,29 @@ Created on Jan 28, 2016
 @author: raydelto
 '''
 
+import mitch_adventure
+
 def start():
-    print "You've picked Ray as your guide. You have to wait for him to write his quest :)";
+    print "Alright, you made a wise decision follow me";
+    print "Ray took the lead and walked into the shadows of the dark night, the silence in creepy";
+    print "You can only listen your footsteps, after a few meters Ray turns back and says"
+    print " -- 'Oh no!', they've found us."
+    print ""
+    print ""
+    print ""
+    print"Time for a decision has arrived.  What would you like to do?"
+    choice = 0
+    while choice not in (1,2,3):
+        choice= input("1- Ask who are them\n2- Run away\n3- Return and pick Mitch as guide for your adventure\n")
+        if choice == 1:
+            print "Oh my God!, what have they done to you?. You don't remember anything, do you?"
+            print "You're John Morrippier world's most famous scientist , you've just discovered the new fuel based on water"
+            print "The big oil companies are after you, if your new invention goes live they're all going to file bankrupcy"
+            print "They have a comsorcium they've kidnapped you two years ago and did terrible things to you"
+        elif choice == 2:
+            print "You ran away, fall to the ground, and suddenly feel a sharpened object stabbing you repeteadly, everything is blurry, everything is black..."
+            print "\nGAME OVER\n"
+        elif choice == 3:
+            mitch_adventure.start()            
+        else:
+            print "Please select a valid option"    
